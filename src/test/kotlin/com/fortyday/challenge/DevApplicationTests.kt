2a88b516df487e86816343a5a36483030194ceb4
dd9971 <daniel.dlugosz@interia.pl>
2025-06-06 07:09:24 +0200

    Initialize Spring Boot project with Kotlin, Gradle and Java 21

---
+++ src/test/kotlin/com/fortyday/challenge/DevApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fortyday.challenge
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DevApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
